afa3ef5174ff99a4284935d1801de3311c87600b
ConsoleXS <aditya.rao.2019.yeet@gmail.com>
2022-06-19 14:32:16 -0500

    When player is in riptide mode, stops rendering items

--- src/main/java/com/consolex/trishieldmod/TriShieldMod.java
+++ src/main/java/com/consolex/trishieldmod/TriShieldMod.java
@@ -12,10 +12,6 @@ public class TriShieldMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("TriShield Initialized!");
 	}
 }
--- src/main/java/com/consolex/trishieldmod/mixin/ExampleMixin.java
+++ src/main/java/com/consolex/trishieldmod/mixin/ExampleMixin.java
@@ -1,16 +1,45 @@
 package com.consolex.trishieldmod.mixin;
 
 import com.consolex.trishieldmod.TriShieldMod;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.model.EntityModelLayers;
+import net.minecraft.client.render.entity.model.ShieldEntityModel;
+import net.minecraft.client.render.entity.model.TridentEntityModel;
+import net.minecraft.client.render.item.BuiltinModelItemRenderer;
+import net.minecraft.client.render.item.HeldItemRenderer;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.json.ModelTransformation;
+import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(TitleScreen.class)
+import java.util.Stack;
+
+@Mixin(HeldItemRenderer.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		TriShieldMod.LOGGER.info("This line is printed by an example mod mixin!");
+
+
+	@Inject(at = @At(value = "HEAD"), method = "renderItem(FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;Lnet/minecraft/client/network/ClientPlayerEntity;I)V", cancellable = true)
+	private void hideShield(float tickDelta, MatrixStack matrices, VertexConsumerProvider.Immediate vertexConsumers, ClientPlayerEntity player, int light, CallbackInfo ci) {
+		ClientPlayerEntity entity = MinecraftClient.getInstance().player;
+		assert entity != null;
+
+		if (entity.isUsingRiptide())
+		{
+			ci.cancel();
+		}
 	}
 }
